45aaacb3291a5e59c0a578291ad00e6ad28d8180
Itay Bittan <itay.bittan@gmail.com>
2012-12-16 21:12:08 +0200

    Voting panel should have the members name as a title

--- src/web/www/javascripts/controllers/memberVotesController.js
+++ src/web/www/javascripts/controllers/memberVotesController.js
@@ -63,8 +63,10 @@ Ext.regController('memberVotes', {
 			});
 		}
 
+		var member = getMembersById(options.id)[0];
+		
 		//Change toolbar title
-		this.application.viewport.query('#toolbar')[0].setTitle(OKnesset.strings.votes);
+		this.application.viewport.query('#toolbar')[0].setTitle(OKnesset.strings.votes + ": " + member.name);
 
 
 
@@ -76,8 +78,6 @@ Ext.regController('memberVotes', {
 				});
 			}
 		}
-
-		var member = getMembersById(options.id)[0];
 
 		// don't track if the panal was reached by pressing 'back'
 		if (options.pushed){
